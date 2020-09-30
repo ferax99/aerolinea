1792564f5df3b21f18d73e37ee6fb19ca02006d8
danieloaiza <37221971+danieloaiza@users.noreply.github.com>
2020-09-30 00:36:49 -0600

    Se agregan las lineas para crear pdf

--- aerolinea.h
+++ aerolinea.h
@@ -1,7 +1,15 @@
 #include <mysql/mysql.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <setjmp.h>
+#include "hpdf.h"
+#include "grid_sheet.h"
 
+jmp_buf env;
 
+void show_desciption(HPDF_Page page, HPDF_REAL x, HPDF_REAL y, const char *text);
+int crear_doc();
 void borrar(char *str);
 void consultar(char * str);
